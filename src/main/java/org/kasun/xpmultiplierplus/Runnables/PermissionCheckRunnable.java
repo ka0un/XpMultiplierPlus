6fa9bd3cbd0a71371d8ac351a2bbcfe08765c7bc
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2023-10-08 08:59:10 +0530

    - when permission removed multiplier get removed too

--- src/main/java/org/kasun/xpmultiplierplus/Runnables/PermissionCheckRunnable.java
+++ src/main/java/org/kasun/xpmultiplierplus/Runnables/PermissionCheckRunnable.java
@@ -5,18 +5,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.kasun.xpmultiplierplus.Multiplier.Multiplier;
 import org.kasun.xpmultiplierplus.Multiplier.MultiplierManager;
+import org.kasun.xpmultiplierplus.Multiplier.TempMultiplier;
 import org.kasun.xpmultiplierplus.XpMultiplierPlus;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class PermissionCheckRunnable extends BukkitRunnable {
 
     private XpMultiplierPlus plugin;
     private List<Multiplier> multipliers;
     private MultiplierManager multiplierManager;
+    private HashMap<UUID, String> tempPermissionMap;
 
     public PermissionCheckRunnable(XpMultiplierPlus plugin) {
         this.plugin = plugin;
+        tempPermissionMap = new HashMap<>();
 
     }
 
@@ -25,14 +30,44 @@ public class PermissionCheckRunnable extends BukkitRunnable {
         multipliers = plugin.getMainManager().getConfigManager().getMainConfig().multipliers;
         multiplierManager = plugin.getMainManager().getMultiplierManager();
 
-
+        // if player has perms added him a multiplier
         for (Player player : Bukkit.getOnlinePlayers()) {
+
+            if (player.isOp()){
+                continue;
+            }
+
             for (Multiplier m : multipliers) {
                 if (player.hasPermission(m.getPermission())) {
                     multiplierManager.getMultipliers().put(player.getUniqueId(), m);
+                    tempPermissionMap.put(player.getUniqueId(), m.getPermission());
                     break;  // Exit the loop once a permission is found
                 }
             }
         }
+
+        //if player doesnt have perms removes his multiplier
+        for (UUID uuid : tempPermissionMap.keySet()) {
+            if (!(Bukkit.getPlayer(uuid) == null)) {
+                if (!(Bukkit.getPlayer(uuid).hasPermission(tempPermissionMap.get(uuid)))) {
+                    multiplierManager.getMultipliers().remove(uuid);
+                    tempPermissionMap.remove(uuid);
+                }
+            }
+        }
+
+        //if temp multiplier outdated removes it
+        for (UUID uuid : multiplierManager.getMultipliers().keySet()) {
+            if (multiplierManager.getMultipliers().get(uuid) instanceof TempMultiplier) {
+                if (((TempMultiplier) multiplierManager.getMultipliers().get(uuid)).getStartTime() != null) {
+                    if (((TempMultiplier) multiplierManager.getMultipliers().get(uuid)).getTimeSecounds() != 0) {
+                        if (((TempMultiplier) multiplierManager.getMultipliers().get(uuid)).getStartTime().getTime() + (((TempMultiplier) multiplierManager.getMultipliers().get(uuid)).getTimeSecounds() * 1000) < System.currentTimeMillis()) {
+                            multiplierManager.getMultipliers().remove(uuid);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
