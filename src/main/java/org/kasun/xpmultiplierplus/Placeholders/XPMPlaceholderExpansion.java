8e9a4872422b9d3c74549d697d6a4f60abd56ace
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2023-10-12 13:13:08 +0530

    - added %xpm_xp_points% placeholder - added %xpm_xp_levels% placeholder - added %xpm_global% placeholder

--- src/main/java/org/kasun/xpmultiplierplus/Placeholders/XPMPlaceholderExpansion.java
+++ src/main/java/org/kasun/xpmultiplierplus/Placeholders/XPMPlaceholderExpansion.java
@@ -2,8 +2,12 @@ package org.kasun.xpmultiplierplus.Placeholders;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
+import org.kasun.xpmultiplierplus.Multiplier.GlobalMultiplier;
+import org.kasun.xpmultiplierplus.Multiplier.MultiplierManager;
 import org.kasun.xpmultiplierplus.Multiplier.MultiplierProvider;
 import org.kasun.xpmultiplierplus.XpMultiplierPlus;
+
+import java.util.ArrayList;
 
 public class XPMPlaceholderExpansion extends PlaceholderExpansion {
     XpMultiplierPlus plugin = XpMultiplierPlus.getInstance();
@@ -28,6 +32,27 @@ public class XPMPlaceholderExpansion extends PlaceholderExpansion {
             MultiplierProvider multiplierProvider = new MultiplierProvider(plugin.getMainManager().getMultiplierManager().getMultipliers());
             return String.valueOf(multiplierProvider.getPlayersBestMultiplier(player.getUniqueId()).getMultiplier());
         }
+
+        if (identifier.equals("global")) {
+            MultiplierManager multiplierManager = plugin.getMainManager().getMultiplierManager();
+            ArrayList<GlobalMultiplier> globalMultipliers = multiplierManager.getGlobalMultipliers();
+            GlobalMultiplier bestGlobalMultiplier = globalMultipliers.get(0);
+            for (GlobalMultiplier globalMultiplier : globalMultipliers) {
+                if (bestGlobalMultiplier.getMultiplier() < globalMultiplier.getMultiplier()) {
+                    bestGlobalMultiplier = globalMultiplier;
+                }
+            }
+            return String.valueOf(bestGlobalMultiplier.getMultiplier());
+        }
+
+        if (identifier.equals("xp_points")){
+            return String.valueOf(player.getTotalExperience());
+        }
+
+        if (identifier.equals("xp_level")){
+            return String.valueOf(player.getLevel());
+        }
+
 
         return null;
     }
